2dba24ed4870976ec966aec4c78038c7f226e1b6
Vusal Huseynov <vslh333222111@gmail.com>
2025-01-03 11:58:46 +0400

    @staticmethod

---
+++ OOP/staticmethod.py
@@ -0,0 +1,17 @@
+class Calculator:
+    @staticmethod
+    def add(x,y):
+        return x + y
+
+    @staticmethod
+    def subtract(x,y):
+        return x - y
+
+# c1 = Calculator()
+
+# result = c1.add(5,4)
+
+# print(result)
+
+print(Calculator.add(3,4))
+print(Calculator.subtract(10,5))
