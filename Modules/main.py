ae2f75d915e5888d20b55830c7d385e81ea61dc6
Vusal Huseynov <vslh333222111@gmail.com>
2025-01-17 16:58:24 +0400

    Modules

---
+++ Modules/main.py
@@ -0,0 +1,60 @@
+"""
+module - Tətbiqinizə daxil etmək istədiyiniz kod toplusunu (məsələn, funksiyalar) ehtiva edən fayl.
+
+Modullar böyük bir proqramı hissələrə bölərək işləmə imkanı verir və təkrarçılığın qarşısını alaraq
+səliqəli/oxunaqlı kod yazmağa təkan verir.
+"""
+
+# import mymodule as m
+
+# print(mymodule.faktorial(5))
+# print(mymodule.pi)
+
+# print(m.faktorial(5))
+# print(m.pi)
+
+# from mymodule import pi, faktorial
+#
+# print(pi)
+# print(faktorial(4))
+
+# from mymodule import *
+
+# print(pi, faktorial(6))
+
+# import mymodule
+#
+# print(mymodule.pi)
+
+# print(help("modules"))
+
+# print(help("math"))
+
+# import math
+
+# print(math.pi)
+# print(math.e)
+
+# print(math.ceil(5.1))
+# print(math.floor(5.9))
+
+# print(math.sqrt(16))
+# print(16 ** 0.5)
+
+
+# import math as m
+#
+# print(m.cos(m.pi))
+
+from math import factorial,sin, pi
+
+# print(sin(pi / 2))
+print(factorial(7))
+
+
+
+
+
+
+
+
---
+++ Modules/mymodule.py
@@ -0,0 +1,19 @@
+def faktorial(x):
+    netice = 1
+
+    for i in range(1, x + 1):
+        netice *= i
+
+    return netice
+
+pi = 3.1415
+
+# print("My Module")
+
+# print(__name__)
+#
+# def main():
+#     print("My Module")
+#
+# if __name__ == "__main__":
+#     main()
