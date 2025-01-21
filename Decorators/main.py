e0aabb1fdd9ec1e02c0281c014915d87a207bf70
Vusal Huseynov <vslh333222111@gmail.com>
2025-01-21 14:39:00 +0400

    Decorators (*args, **kwargs)

--- Decorators/main.py
+++ Decorators/main.py
@@ -16,28 +16,55 @@ Decorator(s) - digər funksiyaların funksionallığını dəyişdirən funksiya
 #
 # ordinary()
 
-import time
+# import time
+#
+# def timer(func):
+#     def wrapper():
+#         t1 = time.time()
+#         print("Start")
+#         func()
+#         print("End")
+#         t2 = time.time()
+#
+#         res = round((t2 - t1) * 1000)
+#
+#         print(f"{func.__name__} executed in {res} milliseconds")
+#
+#     return wrapper
+#
+# @timer
+# def task():
+#     # i = 0
+#     #
+#     # while(i < 100000000):
+#     #     i += 1
+#
+#     time.sleep(3)
+# task()
 
-def timer(func):
-    def wrapper():
-        t1 = time.time()
-        print("Start")
-        func()
-        print("End")
-        t2 = time.time()
 
-        res = round((t2 - t1) * 1000)
+# f(1,2,3,"salam", x = 5, y = 3)
 
-        print(f"{func.__name__} executed in {res} milliseconds")
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Before")
+        res = func(*args, **kwargs)
+        print("After")
+
+        return res
 
     return wrapper
 
-@timer
-def task():
-    # i = 0
-    #
-    # while(i < 100000000):
-    #     i += 1
+@decorator
+def topla(x,y):
+    return x + y
 
-    time.sleep(3)
-task()
+print(topla(5, y = 5))
+
+
+
+
+
+
+
+
